c131c1c11682d2ee46ff228bdb2f14ecdc10c0cd
nseyoum <nseyoum@ucsd.edu>
2022-01-20 19:08:50 -0800

    error found

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,11 +10,14 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        System.out.println("markdown length: " + markdown.length());
         while(currentIndex < markdown.length()) {
+            System.out.println("current index: " + currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            System.out.println("openParen: " + openParen + " closeParen: " + closeParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
